84978b221482efc944722779fb928336fb3fd809
Telmo J Moura <telmop1999@gmail.com>
2024-03-06 19:38:37 +0000

    chore: first commit and Order Object

---
+++ Back-end-Challenge/Data/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace Back_end_Challenge.Data
+{
+    public class Order
+    {
+        public int OrderNumber { get; set; }
+
+        public required List<OrderItem> Itens { get; set; }
+    }
+
+
+}
---
+++ Back-end-Challenge/Data/OrderItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Back_end_Challenge.Data
+{
+    public class OrderItem
+    {
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
