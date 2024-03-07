aa058fc62e4f5ffd81fd94ffdbc5399ff6313845
Telmo J Moura <telmop1999@gmail.com>
2024-03-07 18:36:53 +0000

    chore: Post and Put Functions

--- Back-end-Challenge/Controllers/PedidoController.cs
+++ Back-end-Challenge/Controllers/PedidoController.cs
@@ -1,36 +1,81 @@
-﻿using Back_end_Challenge.Entities;
-using Microsoft.AspNetCore.Http;
+﻿using Back_end_Challenge.Data;
+using Back_end_Challenge.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Back_end_Challenge.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class pedidoController : ControllerBase
+    public class PedidoController : ControllerBase
     {
-        [HttpGet]
-        public async Task<ActionResult<List<Order>>> GetAllOrders()
+        private readonly AppDbContext _appDbContext;
+
+        public PedidoController(AppDbContext appDbContext)
         {
-            var orders = new List<Order>
-            {
-                new Order
-                {
-                    Pedido = 123456,
-                    Itens = new List<OrderItem>
-                    {
-                        new OrderItem
-                        {
-                            Descricao = "Description",
-                            PrecoUnitario = 1,
-                            Qtd = 1,
-                        }
-                    }
-                }
-            };
+            _appDbContext = appDbContext;
+        }
 
-
+        [HttpGet]
+        public async Task<ActionResult<Order>> GetAllPedidos()
+        {
+            var orders = await _appDbContext.Orders.ToListAsync();
 
             return Ok(orders);
         }
+
+        [HttpGet("{numeroPedido}")]
+        public async Task<ActionResult<List<Order>>> GetPedidosByNumero(int numeroPedido)
+        {
+            var order = await _appDbContext.Orders.FindAsync(numeroPedido);
+            if (order is null)
+                return NotFound("CODIGO_PEDIDO_INVALIDO");
+
+            return Ok(order);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Order>>> AddPedido([FromBody] Order order)
+        {
+            _appDbContext.Orders.Add(order);
+            await _appDbContext.SaveChangesAsync();
+
+            return Ok(await _appDbContext.Orders.ToListAsync());
+        }
+
+
+        [HttpPut]
+        public async Task<ActionResult<List<Order>>> UpdatePedido([FromBody] Order UpdatedOrder)
+        {
+            var dbOrder = await _appDbContext.Orders.FindAsync(UpdatedOrder.Pedido);
+
+            if (dbOrder is null)
+                return NotFound("CODIGO_PEDIDO_INVALIDO");
+
+
+
+            dbOrder.Pedido = UpdatedOrder.Pedido;
+
+            foreach (var updatedItem in UpdatedOrder.Itens)
+            {
+                var dbItem = dbOrder.Itens.FirstOrDefault(i => i.Descricao == updatedItem.Descricao);
+
+                if (dbItem != null)
+                {
+                    dbItem.Descricao = updatedItem.Descricao;
+                    dbItem.PrecoUnitario = updatedItem.PrecoUnitario;
+                    dbItem.Qtd = updatedItem.Qtd;
+                }
+                else
+                {
+                    dbOrder.Itens.Add(updatedItem);
+                }
+            }
+
+            await _appDbContext.SaveChangesAsync();
+
+            return Ok(await _appDbContext.Orders.ToListAsync());
+        }
+
     }
 }
